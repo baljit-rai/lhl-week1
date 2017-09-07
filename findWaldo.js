959b6b61cf3be1e4d4b95bae11bfb62ebe9e3f4b
baljit-rai <31259258+baljit-rai@users.noreply.github.com>
2017-09-07 12:35:34 -0600

    Add files via upload
    add callbacks intro exercises

---
+++ findWaldo.js
@@ -0,0 +1,19 @@
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found(arr);   // execute callback
+    }
+  }
+}
+
+function actionWhenFound(arr) {
+  for (var j = 0; j < arr.length; j++) {
+    if (arr[j] === "Waldo") {
+    console.log("Found " + arr[j] + " at index " + j );
+    }
+  }
+}
+
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
